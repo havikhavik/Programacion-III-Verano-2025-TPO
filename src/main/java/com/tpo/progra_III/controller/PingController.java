90c244c6c0d67937c90978880164db754d5a0b67
Lucas Pascual <pascual_lucas1995@hotmail.com>
2025-02-13 19:39:33 -0300

    otro commit

---
+++ src/main/java/com/tpo/progra_III/controller/PingController.java
@@ -0,0 +1,11 @@
+package com.tpo.progra_III.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PingController {
+    @GetMapping ("/ping") public String pingPong(){
+        return "pong";
+    }
+}
