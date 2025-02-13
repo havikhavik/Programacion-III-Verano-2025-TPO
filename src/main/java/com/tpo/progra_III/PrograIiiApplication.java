9816997be3528f730550652de933a043ce56b354
Lucas Pascual <pascual_lucas1995@hotmail.com>
2025-02-13 19:06:09 -0300

    first commit

---
+++ src/main/java/com/tpo/progra_III/PrograIiiApplication.java
@@ -0,0 +1,13 @@
+package com.tpo.progra_III;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrograIiiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrograIiiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tpo/progra_III/PrograIiiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tpo.progra_III;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrograIiiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
